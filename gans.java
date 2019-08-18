61cd24c036d8789a4139f3dc09762d6aef712378
Your Name <vera.pronk@gmail.com>
2019-08-18 21:11:30 +0200

    eerste versie

---
+++ gans.java
@@ -0,0 +1,16 @@
+public class gans {
+   	public static void main(String[] args) {
+ 
+	int vakje = 0;
+	Speler speler1 = new Speler();
+	System.out.println("De " + speler1.kleur + " gans staat op vakje " + vakje);
+   }
+
+}
+
+class Speler {
+	String kleur = "blauwe";
+	}
+
+
+
