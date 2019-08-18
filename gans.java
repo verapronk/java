6ff5559d38ae1c226b9a379b09203400f7fdd1b4
Your Name <vera.pronk@gmail.com>
2019-08-18 22:06:27 +0200

    statisch dobbelen en lopen

--- gans.java
+++ gans.java
@@ -1,16 +1,30 @@
 public class gans {
    	public static void main(String[] args) {
- 
-	int vakje = 0;
+ 	
 	Speler speler1 = new Speler();
-	System.out.println("De " + speler1.kleur + " gans staat op vakje " + vakje);
+	speler1.kleur = "blauwe";
+
+	Speler speler2 = new Speler();
+	speler2.kleur = "groene";
+
+	speler1.dobbelen(1);
+	speler2.dobbelen(2);
    }
 
 }
 
+
 class Speler {
-	String kleur = "blauwe";
+	String kleur;
+
+	void dobbelen(int ogen){
+		System.out.println("De " + kleur + " gans gooit " + ogen);
+		lopen(ogen);
 	}
 
+	void lopen(int vakje){
+		System.out.println("De " + kleur + " gans staat op vakje " + vakje);
+	}
+}
 
 
